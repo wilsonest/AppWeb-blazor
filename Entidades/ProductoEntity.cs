181bbd88726c739f7656d5ad80b71b9b2a321a2b
Wilson Estrada <Wilson.estrada@zibordesarrolladores.com>
2023-01-26 18:50:02 -0500

    Agregar archivos de proyecto.

---
+++ Entidades/ProductoEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entidades
+{
+    public class ProductoEntity
+    {
+        [Key]
+        [StringLength(20)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductoId { get; set; }
+        [StringLength(100)]
+        public string ProductoNombre { get; set; }
+        [StringLength(600)]
+        public string ProductoDescripcion { get; set; }
+        [StringLength(600)]
+        public string ProductoImagen { get; set; }
+
+        public string CategoriaId { get; set; }
+        public CategoriasEntity Categoria { get; set; }
+    }
+}
